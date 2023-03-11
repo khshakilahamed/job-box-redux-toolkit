1b4ccd35351e53715e60386159d96edc1f04ab6b
khshakilahamed <khshakil.ahamed18@gmail.com>
2023-03-11 23:54:12 +0600

    add jobs

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -3,8 +3,10 @@ import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithEmailAndP
 import auth from "../../firebase/firebase.config";
 
 const initialState = {
-    email: "",
-    role: "",
+    user: {
+        email: "",
+        role: "",
+    },
     isLoading: true,
     isError: false,
     error: "",
@@ -12,20 +14,27 @@ const initialState = {
 
 export const createUser = createAsyncThunk("auth/createUser", async ({ email, password }, thunkAPI) => {
     const data = await createUserWithEmailAndPassword(auth, email, password);
-
     return data.user.email;
+});
+
+export const getUser = createAsyncThunk("auth/getUser", async (email) => {
+    const res = await fetch(`${process.env.REACT_APP_DEV_URL}user/${email}`);
+    const data = await res.json();
+
+    if (data.status) {
+        return data;
+    }
+    return email;
 });
 
 export const loginUser = createAsyncThunk("auth/loginUser", async ({ email, password }, thunkAPI) => {
     const data = await signInWithEmailAndPassword(auth, email, password);
-
     return data.user.email;
 });
 
 export const googleLogin = createAsyncThunk("auth/googleLogin", async () => {
     const googleProvider = new GoogleAuthProvider();
     const data = await signInWithPopup(auth, googleProvider);
-
     return data.user.email;
 });
 
@@ -34,10 +43,10 @@ const authSlice = createSlice({
     initialState,
     reducers: {
         logout: (state, action) => {
-            state.email = "";
+            state.user.email = "";
         },
         setUser: (state, action) => {
-            state.email = action.payload;
+            state.user.email = action.payload;
             state.isLoading = false;
         },
         toggleLoading: (state, action) => {
@@ -53,13 +62,13 @@ const authSlice = createSlice({
             })
             .addCase(createUser.fulfilled, (state, action) => {
                 state.isLoading = false;
-                state.email = action.payload;
+                state.user.email = action.payload;
                 state.isError = false;
                 state.error = "";
             })
             .addCase(createUser.rejected, (state, action) => {
                 state.isLoading = false;
-                state.email = "";
+                state.user.email = "";
                 state.isError = true;
                 state.error = action.error.message;
             })
@@ -70,13 +79,13 @@ const authSlice = createSlice({
             })
             .addCase(loginUser.fulfilled, (state, action) => {
                 state.isLoading = false;
-                state.email = action.payload;
+                state.user.email = action.payload;
                 state.isError = false;
                 state.error = "";
             })
             .addCase(loginUser.rejected, (state, action) => {
                 state.isLoading = false;
-                state.email = "";
+                state.user.email = "";
                 state.isError = true;
                 state.error = action.error.message;
             })
@@ -87,13 +96,35 @@ const authSlice = createSlice({
             })
             .addCase(googleLogin.fulfilled, (state, action) => {
                 state.isLoading = false;
-                state.email = action.payload;
+                state.user.email = action.payload;
                 state.isError = false;
                 state.error = "";
             })
             .addCase(googleLogin.rejected, (state, action) => {
                 state.isLoading = false;
-                state.email = "";
+                state.user.email = "";
+                state.isError = true;
+                state.error = action.error.message;
+            })
+            .addCase(getUser.pending, (state, action) => {
+                state.isLoading = true;
+                state.isError = false;
+                state.error = "";
+            })
+            .addCase(getUser.fulfilled, (state, action) => {
+                state.isLoading = false;
+                if (action.payload.status) {
+                    state.user = action.payload.data;
+                }
+                else {
+                    state.user.email = action.payload;
+                }
+                state.isError = false;
+                state.error = "";
+            })
+            .addCase(getUser.rejected, (state, action) => {
+                state.isLoading = false;
+                state.user.email = "";
                 state.isError = true;
                 state.error = action.error.message;
             })
--- src/pages/register/EmployerRegistration.js
+++ src/pages/register/EmployerRegistration.js
@@ -3,11 +3,17 @@ import { useForm, useWatch } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { FaChevronLeft } from "react-icons/fa";
 import { useRegisterMutation } from "../../features/auth/authApi";
+import { useSelector } from "react-redux";
 
 const EmployerRegistration = () => {
-  const [countries, setCountries] = useState([]);
+  const { user: { email } } = useSelector((state) => state.auth);
+  const { handleSubmit, register, control } = useForm({
+    defaultValues: {
+      email,
+    }
+  });
 
-  const { handleSubmit, register, control } = useForm();
+  const [countries, setCountries] = useState([]);
   const term = useWatch({ control, name: "term" });
   const navigate = useNavigate();
   const [postUser, { isLoading, isError }] = useRegisterMutation();
@@ -77,7 +83,7 @@ const EmployerRegistration = () => {
             <label className='mb-2' htmlFor='email'>
               Email
             </label>
-            <input type='email' id='email' {...register("email")} />
+            <input  className="cursor-not-allowed" type='email' id='email' disabled {...register("email")} />
           </div>
           <div className='flex flex-col w-full max-w-xs'>
             <h1 className='mb-3'>Gender</h1>
